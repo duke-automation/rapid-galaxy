c3064fa46734f063e423edab2632ada9ec1f7ef3
drews@duke.edu <drews@duke.edu>
2016-10-03 09:39:02 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+
+INVENTORY=~/.rapid_ansible_hosts
+
+echo localhost ansible_connection=local > $INVENTORY
+
+## Do the install here
+ansible-playbook -i $INVENTORY ./galaxy.yaml | tee /tmp/rapid_image_status.txt
+
+if [[ ${PIPESTATUS[0]} == 0 ]]; then
+    echo "Ansible Playbook Succeeded"
+    echo "success" > /tmp/rapid_image_complete
+else
+    echo "Ansible playbook failed"
+    echo "fail" > /tmp/rapid_image_complete
+fi
